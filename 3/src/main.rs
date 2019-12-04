813a09e791498f840c6bb01541e67d7d3593cead
Nathan Jhaveri <nathanj@porch.com>
2019-12-03 16:42:30 -0800

    3.2 finished

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,11 +1,12 @@
 use std::error::Error;
 use std::fs::read_to_string;
-use std::cmp::Ordering;
+use std::cmp;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 struct Point {
     pub x: i32,
     pub y: i32,
+    travel_steps: u32
 }
 
 impl Point {
@@ -25,52 +26,57 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let distance = find_distance(input)?;
 
-    println!("distnace: {}", distance);
+    println!("distance: {}", distance);
 
     Ok(())
 }
 
-fn find_distance(input: String) -> Result<i32, Box<dyn Error>> {
+fn find_distance(input: String) -> Result<u32, Box<dyn Error>> {
     let mut segments = input.split("\n");
     let wire1 = wire_segments(segments.next().unwrap());
     let wire2 = wire_segments(segments.next().unwrap());
 
     let mut intersections = find_intersections(wire1, wire2);
-    intersections.sort_by(|a, b| a.distance().cmp(&b.distance()));
+    intersections.sort_by(|a, b| a.travel_steps.cmp(&b.travel_steps));
     let first = intersections.first().unwrap();
 
-    println!("first {}", intersections.first().unwrap().distance());
-    println!("last {}", intersections.last().unwrap().distance());
+    println!("first {:?}", intersections.first().unwrap());
 
-    Ok(first.distance())
+    Ok(first.travel_steps)
 }
 
 
 fn wire_segments(line: &str) -> Vec<Line> {
     let mut x = 0;
     let mut y = 0;
+    let mut acc: u32 = 0;
 
     let segments: Vec<Line> = line
         .split(",")
         .map(|op| -> Line {
             let count: i32 = op[1..].parse().unwrap();
             let dir = &op[0..1];
-            let (startx, starty, endx, endy, newx, newy) = match dir {
-                "U" => (x, y, x, y + count, x, y + count),
-                "D" => (x, y - count, x, y, x, y - count),
-                "R" => (x, y, x + count, y, x + count, y),
-                "L" => (x - count, y, x, y, x - count, y),
+            let (endx, endy)  = match dir {
+                "U" => (x, y + count),
+                "D" => (x, y - count),
+                "R" => (x + count, y),
+                "L" => (x - count, y),
                 _ => panic!("unexpected direction"),
             };
 
-            // Update current cursor position
-            x = newx;
-            y = newy;
+            let travel_steps = acc + count as u32;
 
-            Line {
-                start: Point { x: startx, y: starty },
-                end: Point { x: endx, y: endy },
-            }
+            // Update current cursor position
+            let segment = Line {
+                start: Point { x, y, travel_steps: acc },
+                end: Point { x: endx, y: endy, travel_steps },
+            };
+
+            x = endx;
+            y = endy;
+            acc = travel_steps;
+
+            return segment;
         })
         .collect();
 
@@ -113,9 +119,17 @@ fn intersection(line1: &Line, line2: &Line) -> Option<Point> {
             return None
         }
 
-        if vert.start.y <= y && y <= vert.end.y && horz.start.x <= x && x <= horz.end.x {
-            //println!("found intersecton {:?} {:?}, point: {:?}", horz, vert, Point { x, y });
-            return Some(Point { x, y })
+        let miny = cmp::min(vert.start.y, vert.end.y);
+        let maxy = cmp::max(vert.start.y, vert.end.y);
+        let minx = cmp::min(horz.start.x, horz.end.x);
+        let maxx = cmp::max(horz.start.x, horz.end.x);
+
+        if miny <= y && y <= maxy && minx <= x && x <= maxx {
+            let travel_steps: u32 = horz.start.travel_steps + vert.start.travel_steps + ((horz.start.x - x).abs() + (vert.start.y - y).abs()) as u32;
+            let overlap = Point { x, y, travel_steps, };
+
+            println!("found intersecton\nLine1: {:?}\nLine2: {:?}\npoint: {:?}", horz, vert, overlap);
+            return Some(overlap)
         }
     }
 
@@ -136,9 +150,9 @@ mod tests {
 
     #[test]
     fn find_itersection() {
-        let origin = Point { x: 0, y: 0 };
-        let y = Point { x: 0, y: 2 };
-        let x = Point { x: 2, y: 0 };
+        let origin = Point { x: 0, y: 0, travel_steps: 0 };
+        let y = Point { x: 0, y: 2, travel_steps: 0, };
+        let x = Point { x: 2, y: 0, travel_steps: 0, };
         let line1 = Line { start: origin, end: y };
         let line2 = Line { start: origin, end: x };
 
@@ -160,7 +174,7 @@ mod tests {
         let input: String = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83".to_string();
         let distance = find_distance(input).unwrap();
 
-        assert_eq!(159, distance);
+        assert_eq!(610, distance);
     }
 }
 
