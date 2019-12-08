936d7da885c18eae0cf3f7b36babfd56cd746031
Nathan Jhaveri <jhaveri@umich.edu>
2019-12-07 22:20:21 -0800

    Solve 7.2

--- 7/src/lib.rs
+++ 7/src/lib.rs
@@ -1,8 +1,9 @@
-use intcode::{compute, Ops};
+use intcode::{IntCode, Ops};
 use std::ops::Range;
 
 const AMP_COUNT: usize = 5;
 const PHASE_RANGE: Range<i32> = 0..5;
+const PHASE_RANGE_REPEAT: Range<i32> = 5..10;
 pub type PhaseSettings = [i32; AMP_COUNT];
 
 pub fn find_max_phase_setting(program: &Ops) -> (i32, PhaseSettings) {
@@ -34,10 +35,14 @@ pub fn find_max_phase_setting(program: &Ops) -> (i32, PhaseSettings) {
 fn thruster_signal(program: &Ops, phase_settings: &PhaseSettings) -> i32 {
     let mut input_signal = 0;
     for i in 0..AMP_COUNT {
-        let mut running = program.clone();
+        let running = program.clone();
         let program_input = [phase_settings[i], input_signal];
-        let output = compute(&mut running, &program_input);
-        input_signal = *output.last().expect("No ouput")
+        let mut computer = IntCode::init(running);
+        for &input in program_input.iter() {
+            computer.input(input);
+        }
+
+        input_signal = computer.compute_output().expect("no output");
     }
 
     input_signal
@@ -52,10 +57,66 @@ fn valid_phase_setting(phase: &PhaseSettings) -> bool {
     contains_all
 }
 
+fn valid_feedback_phase_setting(phase: &PhaseSettings) -> bool {
+    let mut contains_all = true;
+    for setting in PHASE_RANGE_REPEAT {
+        contains_all &= phase.contains(&setting);
+    }
+
+    contains_all
+}
+
+fn feedback(program: &str, phase: &PhaseSettings) -> i32 {
+    let mut output = Some(0);
+    let mut amps: Vec<IntCode> = (0..AMP_COUNT).map(|_| IntCode::new(program)).collect();
+
+    // Initialize phases settings
+    for i in 0..AMP_COUNT {
+        amps[i].input(phase[i]);
+    }
+
+    let mut amp_index = 0;
+    while let Some(input) = output {
+        let amp = &mut amps[amp_index];
+        amp.input(input);
+        output = amp.compute_output();
+        amp_index = (amp_index + 1) % AMP_COUNT;
+    }
+
+    amps[AMP_COUNT - 1].last_output()
+}
+
+pub fn find_max_feedback_phase_setting(program: &str) -> (i32, PhaseSettings) {
+    let mut max_thruster = 0;
+    let mut phase_settings: PhaseSettings = [0; AMP_COUNT];
+
+    for a in PHASE_RANGE_REPEAT {
+        for b in PHASE_RANGE_REPEAT {
+            for c in PHASE_RANGE_REPEAT {
+                for d in PHASE_RANGE_REPEAT {
+                    for e in PHASE_RANGE_REPEAT {
+                        let test_settings = [a, b, c, d, e];
+                        if valid_feedback_phase_setting(&test_settings) {
+                            let test_signal = feedback(program, &test_settings);
+                            if max_thruster < test_signal {
+                                max_thruster = test_signal;
+                                phase_settings = test_settings;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    (max_thruster, phase_settings)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use intcode::init_ops;
+    const PROGRAM_7: &str = "3,8,1001,8,10,8,105,1,0,0,21,34,47,72,81,102,183,264,345,426,99999,3,9,102,5,9,9,1001,9,3,9,4,9,99,3,9,101,4,9,9,1002,9,3,9,4,9,99,3,9,102,3,9,9,101,2,9,9,102,5,9,9,1001,9,3,9,1002,9,4,9,4,9,99,3,9,101,5,9,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,101,4,9,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99";
 
     #[test]
     fn example1() {
@@ -82,9 +143,8 @@ mod tests {
     }
 
     #[test]
-    fn seven_one() {
-        let ops = "3,8,1001,8,10,8,105,1,0,0,21,34,47,72,81,102,183,264,345,426,99999,3,9,102,5,9,9,1001,9,3,9,4,9,99,3,9,101,4,9,9,1002,9,3,9,4,9,99,3,9,102,3,9,9,101,2,9,9,102,5,9,9,1001,9,3,9,1002,9,4,9,4,9,99,3,9,101,5,9,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,101,4,9,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99";
-        let program = init_ops(ops).unwrap();
+    fn seven_1() {
+        let program = init_ops(PROGRAM_7).unwrap();
         let expected_max = 92663;
         let expected_phase: PhaseSettings = [3, 1, 4, 2, 0];
 
@@ -92,5 +152,36 @@ mod tests {
 
         assert_eq!(actual_phase, expected_phase);
         assert_eq!(actual_max, expected_max);
+    }
+
+    #[test]
+    fn seven_2() {
+        let expected_max = 14_365_052;
+        let expected_phase: PhaseSettings = [7, 8, 6, 9, 5];
+        let (actual_max, actual_phase) = find_max_feedback_phase_setting(PROGRAM_7);
+
+        assert_eq!(actual_max, expected_max);
+        assert_eq!(actual_phase, expected_phase);
+    }
+
+    #[test]
+    fn example_2_1() {
+        let program =
+            "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
+        let phase = [9, 8, 7, 6, 5];
+
+        let output = feedback(program, &phase);
+        assert_eq!(output, 139_629_729);
+    }
+
+    #[test]
+    fn example_2_1_find() {
+        let program =
+            "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
+        let expected_phase = [9, 8, 7, 6, 5];
+
+        let (max_thruster, phase_setting) = find_max_feedback_phase_setting(program);
+        assert_eq!(max_thruster, 139_629_729);
+        assert_eq!(phase_setting, expected_phase);
     }
 }
--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -46,6 +46,138 @@ fn resolve_op(ops: &Ops, index: usize, mode: OpMode) -> i32 {
     }
 }
 
+pub struct IntCode {
+    ops: Ops,
+    ip: usize,
+    input_pos: usize,
+    input: Vec<i32>,
+    output: Vec<i32>,
+}
+
+impl IntCode {
+    pub fn new(program: &str) -> IntCode {
+        let ops = init_ops(program).expect("Failed parsing program");
+        IntCode::init(ops)
+    }
+
+    pub fn init(ops: Ops) -> IntCode {
+        IntCode {
+            ops: ops,
+            ip: 0,
+            input_pos: 0,
+            input: Vec::new(),
+            output: Vec::new(),
+        }
+    }
+
+    pub fn input(&mut self, num: i32) {
+        self.input.push(num);
+    }
+
+    pub fn last_output(&self) -> i32 {
+        *self.output.last().expect("No output")
+    }
+
+    fn run(&mut self) -> () {
+        while let Some(out) = self.compute_output() {
+            self.output.push(out);
+        }
+    }
+
+    pub fn compute_output(&mut self) -> Option<i32> {
+        let ops = &mut self.ops;
+
+        loop {
+            let coded_op = ops[self.ip];
+            let (op, mode1, mode2, _mode3) = parse_op(coded_op);
+            self.ip += 1;
+
+            if op == HCF {
+                return None;
+            }
+
+            match op {
+                ADD => {
+                    let a = resolve_op(ops, self.ip, mode1);
+                    self.ip += 1;
+                    let b = resolve_op(ops, self.ip, mode2);
+                    self.ip += 1;
+                    let rp = ops[self.ip] as usize;
+                    self.ip += 1;
+
+                    ops[rp] = a + b;
+                }
+                MULTIPLY => {
+                    let a = resolve_op(ops, self.ip, mode1);
+                    self.ip += 1;
+                    let b = resolve_op(ops, self.ip, mode2);
+                    self.ip += 1;
+                    let rp = ops[self.ip] as usize;
+                    self.ip += 1;
+
+                    ops[rp] = a * b;
+                }
+                INPUT => {
+                    let input_storage_index = ops[self.ip] as usize;
+                    ops[input_storage_index] = self.input[self.input_pos];
+                    self.input_pos += 1;
+                    self.ip += 1
+                }
+                OUTPUT => {
+                    let input_index = ops[self.ip] as usize;
+                    self.ip += 1;
+                    let out = ops[input_index];
+                    self.output.push(out);
+                    return Some(out);
+                }
+                JUMP_IF_TRUE => {
+                    let a = resolve_op(ops, self.ip, mode1);
+                    self.ip += 1;
+                    let b = resolve_op(ops, self.ip, mode2);
+                    self.ip += 1;
+
+                    if a != 0 {
+                        self.ip = b as usize;
+                    }
+                }
+                JUMP_IF_FALSE => {
+                    let a = resolve_op(ops, self.ip, mode1);
+                    self.ip += 1;
+                    let b = resolve_op(ops, self.ip, mode2);
+                    self.ip += 1;
+
+                    if a == 0 {
+                        self.ip = b as usize;
+                    }
+                }
+                LESS_THAN => {
+                    let a = resolve_op(ops, self.ip, mode1);
+                    self.ip += 1;
+                    let b = resolve_op(ops, self.ip, mode2);
+                    self.ip += 1;
+
+                    let rp = ops[self.ip] as usize;
+                    self.ip += 1;
+
+                    ops[rp] = if a < b { 1 } else { 0 };
+                }
+                EQUALS => {
+                    let a = resolve_op(ops, self.ip, mode1);
+                    self.ip += 1;
+                    let b = resolve_op(ops, self.ip, mode2);
+                    self.ip += 1;
+
+                    let rp = ops[self.ip] as usize;
+                    self.ip += 1;
+
+                    ops[rp] = if a == b { 1 } else { 0 };
+                }
+                _ => panic!("Unrecognized instruction {}", op),
+            };
+        }
+    }
+}
+
 pub fn compute(ops: &mut Ops, input: &[i32]) -> Vec<i32> {
     let mut ip = 0; // Instruction pointer
     let mut output = Vec::new();
@@ -159,11 +291,10 @@ mod tests {
     const PROGRAM_5: &str = "3,225,1,225,6,6,1100,1,238,225,104,0,101,67,166,224,1001,224,-110,224,4,224,102,8,223,223,1001,224,4,224,1,224,223,223,2,62,66,224,101,-406,224,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1101,76,51,225,1101,51,29,225,1102,57,14,225,1102,64,48,224,1001,224,-3072,224,4,224,102,8,223,223,1001,224,1,224,1,224,223,223,1001,217,90,224,1001,224,-101,224,4,224,1002,223,8,223,1001,224,2,224,1,223,224,223,1101,57,55,224,1001,224,-112,224,4,224,102,8,223,223,1001,224,7,224,1,223,224,223,1102,5,62,225,1102,49,68,225,102,40,140,224,101,-2720,224,224,4,224,1002,223,8,223,1001,224,4,224,1,223,224,223,1101,92,43,225,1101,93,21,225,1002,170,31,224,101,-651,224,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1,136,57,224,1001,224,-138,224,4,224,102,8,223,223,101,2,224,224,1,223,224,223,1102,11,85,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1107,226,226,224,102,2,223,223,1006,224,329,1001,223,1,223,1007,226,677,224,1002,223,2,223,1005,224,344,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,359,101,1,223,223,1008,226,226,224,1002,223,2,223,1005,224,374,1001,223,1,223,108,677,226,224,1002,223,2,223,1006,224,389,101,1,223,223,7,226,226,224,102,2,223,223,1006,224,404,101,1,223,223,7,677,226,224,1002,223,2,223,1005,224,419,101,1,223,223,107,226,226,224,102,2,223,223,1006,224,434,1001,223,1,223,1008,677,677,224,1002,223,2,223,1005,224,449,101,1,223,223,108,226,226,224,102,2,223,223,1005,224,464,1001,223,1,223,1108,226,677,224,1002,223,2,223,1005,224,479,1001,223,1,223,8,677,226,224,102,2,223,223,1006,224,494,1001,223,1,223,1108,677,677,224,102,2,223,223,1006,224,509,1001,223,1,223,1007,226,226,224,1002,223,2,223,1005,224,524,1001,223,1,223,7,226,677,224,1002,223,2,223,1005,224,539,1001,223,1,223,8,677,677,224,102,2,223,223,1005,224,554,1001,223,1,223,107,226,677,224,1002,223,2,223,1006,224,569,101,1,223,223,1107,226,677,224,102,2,223,223,1005,224,584,1001,223,1,223,1108,677,226,224,102,2,223,223,1006,224,599,1001,223,1,223,1008,677,226,224,102,2,223,223,1006,224,614,101,1,223,223,107,677,677,224,102,2,223,223,1006,224,629,1001,223,1,223,1107,677,226,224,1002,223,2,223,1005,224,644,101,1,223,223,8,226,677,224,102,2,223,223,1005,224,659,1001,223,1,223,1007,677,677,224,102,2,223,223,1005,224,674,1001,223,1,223,4,223,99,226";
 
     fn run_program(program: &str, input: i32) -> i32 {
-        let mut ops = init_ops(program).unwrap();
-        let read = vec![input];
-        let output = compute(&mut ops, &read);
-
-        *output.last().expect("No ouput")
+        let mut computer = IntCode::new(program);
+        computer.input(input);
+        computer.run();
+        computer.last_output()
     }
 
     #[test]
